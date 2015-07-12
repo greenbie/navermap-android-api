ab990a4b020928201b51b4c635e4c7b27ab2b285
Noh Jaechun <horseriding.king@navercorp.com>
2015-07-12 17:37:31 +0900

    샘플 프로젝트 업로드

---
+++ app/src/main/java/com/nhn/android/mapviewer/FragmentActivity.java
@@ -0,0 +1,28 @@
+package com.nhn.android.mapviewer;
+
+import com.nhn.android.maps.NMapView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentActivity extends Activity {
+	
+	private NMapView mMapView;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.framents);
+        
+        mMapView = (NMapView)findViewById(R.id.mapView);
+        
+		// initialize map view
+		mMapView.setClickable(true);
+		mMapView.setEnabled(true);
+		mMapView.setFocusable(true);
+		mMapView.setFocusableInTouchMode(true);
+		mMapView.requestFocus();
+    }
+}
